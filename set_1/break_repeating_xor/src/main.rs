17be91242983b12056fc85d49c713a174379ca82
supinie <86788874+supinie@users.noreply.github.com>
2025-03-07 18:40:35 +0000

    continues challenge 6

--- set_1/break_repeating_xor/src/main.rs
+++ set_1/break_repeating_xor/src/main.rs
@@ -1,4 +1,6 @@
 use crypto_library::{hamming_distance, test_hamming_distance};
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
     let x: &str = "this is a test";
@@ -9,4 +11,22 @@ fn main() {
 
     let test = test_hamming_distance(x, 3);
     println!("{test}");
+
+    let mut challenge = String::new();
+
+    match File::open("src/challenge.txt") {
+        Ok(mut file) => {
+            file.read_to_string(&mut challenge).unwrap();
+        }
+        Err(error) => {
+            panic!("Error opening file: {}", error);
+        }
+    };
+
+    let mut distances: Vec<(f64, usize)> = Vec::new();
+    for keysize in 2..40 {
+        distances.push((test_hamming_distance(&challenge, keysize), keysize));
+    }
+
+    // pick every keysize-th element, keysize-th + 1 etc. and break single xor
 }
